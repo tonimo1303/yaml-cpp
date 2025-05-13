b194fbe9985227ff727261707674339d15fd8cc5
Antonio Flüchter <antonio.fluechter@tonimosoftware.com>
2025-05-13 19:16:49 +0200

    Define YAML as static lib

--- premake5.lua
+++ premake5.lua
@@ -1,8 +1,14 @@
 project "yaml-cpp"
     kind "StaticLib"
+    staticruntime "on"
 
     language "c++"
     cppdialect "c++17"
+
+    defines
+    {
+        "YAML_CPP_STATIC_DEFINE"
+    }
 
     includedirs
     {
